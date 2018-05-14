a51fc37d7a4915c8585e1212dd79e98d709b1e88
laoboxie <594502135@qq.com>
2018-05-14 23:03:31 +0800

    ignore

--- backEnd/apps/goods/filters.py
+++ backEnd/apps/goods/filters.py
@@ -7,7 +7,7 @@ from .models import Goods
 class GoodsFilter(rest_framework.FilterSet):
     pricemin = rest_framework.NumberFilter(name="shop_price", lookup_expr='gte')
     pricemax = rest_framework.NumberFilter(name="shop_price", lookup_expr='lte')
-    top_category = rest_framework.NumberFilter(method="category_filter")
+    top_category = rest_framework.NumberFilter(name="category_id",method="category_filter")
 
     def category_filter(self, queryset, name, value):
         return queryset.filter(Q(category_id=value) | Q(category__parent_category_id=value) |
